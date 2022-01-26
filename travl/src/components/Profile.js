dc54106ae82b2a1fe02b4f8cd6b38e820f609a48
Pier Davis <pieramelia2@gmail.com>
2022-01-26 17:34:43 -0500

    profile progress

--- travl/src/components/Profile.js
+++ travl/src/components/Profile.js
@@ -9,14 +9,12 @@ function Profile() {
     <div class='wrapper'>
         <div class='main'>
             <div className='main-icon'>
-      {/* <div className='circle'></div> */}
-                <img src={icon} className="profile-logo" alt="profile icon" />
-                <img src={logo} className="Travl-logo" alt="logo" /> 
-                <img src={iconBar} id="icon-bar" alt="profile bar" />
+            <img src={logo} className="Travl-logo" alt="logo"/> 
+            <img src={icon} id="profile-logo" alt="profile icon"/>
+            <img src={iconBar} id="icon-bar" alt="profile bar"/>
         </div>
             
     <div className='main-settings'>
-          
         <div id='container' className="red">
             <div className="circlea"></div>
             <div className="circlea"></div>
@@ -27,9 +25,15 @@ function Profile() {
             <div className="circlea"></div>
             <div className="circlea"></div>
             <div className="circlea"></div>
+            <div className="circlea"></div>
+            <div className="circlea"></div>
+            <div className="circlea"></div>
+            <div className="circlea"></div>
+            <div className="circlea"></div>
+            <div className="circlea"></div>
         </div> 
-
     </div>
+    <h2>text</h2>
 </div>
 </div> 
 
